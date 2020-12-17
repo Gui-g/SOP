45ae51f7ac9af0be197001460c835ac8aa090b1a
Gui-g <guilira94@gmail.com>
2020-12-17 17:41:36 -0300

    trab

--- SOP_TRAB1_res.c
+++ SOP_TRAB1_res.c
@@ -6,6 +6,10 @@
 #include <sched.h>
 
 sem_t semA;
+sem_t semBC1;
+sem_t semBC2;
+sem_t semBC3;
+int value;
 
 void* A(void *args);
 void* B(void *args);
@@ -13,6 +17,8 @@ void* C(void *args);
 int main() {
 
 sem_init(&semA, 0, 0);
+sem_init(&semBC1, 0, 0);
+sem_init(&semBC2, 0, 0);
 
 pthread_t threads[3];
 int i;
@@ -30,19 +36,31 @@ return 0;
 
 void* A(void *args) {
     while(1) {
+        sem_wait(&semBC1);
         printf("O");
+        sem_post(&semA);
+        sem_wait(&semBC2);
     }
 }
 
 void* B(void *args) {
     while(1) {
         printf("CA");
+        sem_post(&semBC1);
+        sem_post(&semBC1);
+        sem_wait(&semA);
+        sem_wait(&semA);
         printf("!\n");
+        sem_post(&semBC2);
+        sem_post(&semBC2);
     }
 }
 
 void* C(void *args) {
     while(1) {
+        sem_wait(&semBC1);
         printf("S");
+        sem_post(&semA);
+        sem_wait(&semBC2);
     }
 }
